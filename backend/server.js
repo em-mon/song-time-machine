b53b76bded9067f5b9843162b8d58e558dd81e51
em-mon <emmamarimontero@gmail.com>
2025-12-26 21:00:53 -0600

    CORS issue

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,8 @@ const app = express()
 app.use(express.json())
 app.use(cors({
   origin: "https://song-time-machine.vercel.app",
+  methods: ["GET", "POST", "OPTIONS"],
+  allowedHeaders: ["Content-Type"],
   credentials: true,
 }))
 
